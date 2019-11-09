3ec4db6017d45a7d5e1ec35c69236944088fa068
acingae <lontost@gmail.com>
2019-11-09 15:06:53 +0700

    Keeping-Up-With-the-Javascripts-master, homework #1

---
+++ song.js
@@ -0,0 +1,19 @@
+// Define song details using var
+var trackTitle = "Good Night Good Bye"
+var artist = "Tjahjo Wisanggeni"
+var album = "From The Other Side"
+var released = "1992"
+var songLengthInSeconds = 360
+var isUnderRated = true
+var songGenre = ["Instrumental Rock", "	Neoclassical Metal"]
+var songWriters = "Tjahjo Wisanggeni"
+
+// Output each var(s) using console.log
+console.log( "Track title: " + trackTitle, 
+"Artist: " + artist, 
+"Album: " + album, 
+"Released: " + released, 
+"Song duration: " + songLengthInSeconds + " seconds", 
+"Under rated: " + isUnderRated, 
+"Song Genre: " + songGenre, 
+"Song writer(s):" + songWriters)
